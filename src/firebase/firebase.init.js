743c554f1956d06623bcaf87f4d54d42dbf1862f
SAMIA-TASMIN <novaira489@gmail.com>
2025-12-25 19:33:05 +0600

    client setup

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBu8lHm5ZBTZ_eU7MBkIbe2saQaPbxMSUE",
+  authDomain: "ai-inventory-management-2575b.firebaseapp.com",
+  projectId: "ai-inventory-management-2575b",
+  storageBucket: "ai-inventory-management-2575b.firebasestorage.app",
+  messagingSenderId: "1057029489716",
+  appId: "1:1057029489716:web:c53d603f07bd8360fbb661"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
